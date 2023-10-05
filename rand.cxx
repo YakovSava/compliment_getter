4353cdbce3861f2f3fad6ad6593bd0990ccc8cc3
Yakov Savelev (Desktop; Linux) <g6h6m238929@gmail.com>
2023-10-05 18:20:59 +0500

    Test version 1.4

--- rand.cxx
+++ rand.cxx
@@ -1,7 +1,29 @@
 # include <Python.h>
 # include <random>
+using namespace std;
+
+class Rander {
+public:
+    Rander() {
+        random_device rd;
+
+        this->gen(rd());
+    }
+
+    int rand(int range_min, int range_max) {
+        uniform_int_distibution<> dis(range_min, range_max);
+    
+        return dis(this->gen);
+    }
+private:
+    mt19937 gen;
+}
 
 
+typedef struct {
+    PyObject_HEAD,
+    Rander* obj;
+} RanderObj;
 
 
 static PyMethodDef methods[] = {
