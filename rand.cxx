74f4760d2d7d43069e018aa18391f53877a9bcbb
Yakov Savelev (Desktop; Linux) <g6h6m238929@gmail.com>
2023-10-05 18:09:50 +0500

    Test version 1.3

--- rand.cxx
+++ rand.cxx
@@ -1,29 +1,11 @@
-# include <stdlib.h>
 # include <Python.h>
+# include <random>
 
-static PyObject* random(PyObject* self, PyObject* args) {
-    int range_max, range_min;
 
-    if (!PyArg_ParseTuple(args, "ii", &range_min, &range_max)) {
-        return NULL;
-    }
-
-    int result = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
-
-    return Py_BuildValue("i", result);
-}
-
-static PyObject* _overclocking(PyObject* self, PyObject* args) {
-
-    srand(time(NULL));
-
-    return Py_None;
-}
 
 
 static PyMethodDef methods[] = {
     {"random", random, METH_VARARGS, "C/C++ random"},
-    {"_overclocking", _overclocking, METH_VARARGS, "Overclocking"}
     {NULL, NULL, 0, NULL} 
 };
 
